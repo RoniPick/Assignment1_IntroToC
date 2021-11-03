20661b18332475398b723de4cc34969efe54ef30
Netanel Levine <74298433+netanellevine@users.noreply.github.com>
2021-11-03 16:47:32 +0200

    Update main.c

--- main.c
+++ main.c
@@ -5,18 +5,7 @@ int main() {
     int start = 0, end = 0, i;
 //    printf("Enter the first number: \n");
     scanf("%d", &start);
-//    printf("Enter the second number: \n");
     scanf("%d", &end);
-//    int j = 100;
-//    while (start > end && j > 0) {
-//        printf("The first number must be smaller than the second number!!\n");
-//        printf("Please try again.\n");
-//        printf("Enter the first number: \n");
-//        scanf("%d", &start);
-//        printf("Enter the second number: \n");
-//        scanf("%d", &end);
-//        j--;
-//    }
     printf("The Armstrong numbers are:");
     for(i = start; i <= end; i++ ){
         if(isArmstrong(i) == TRUE){
